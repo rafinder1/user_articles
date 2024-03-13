a9ed1253d8fddc9dfa42375f76cc1f460c7e14cb
rafinder1 <rafpro33@gmail.com>
2024-03-13 18:49:57 +0100

    added retrofit

--- lib/data/remote_data_sources/articles_remote_data_source.dart
+++ lib/data/remote_data_sources/articles_remote_data_source.dart
@@ -1,15 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import 'package:user_articles/domain/models/article_model.dart';
 
-class ArticlesRemoteDioDataSource {
-  Future<List<Map<String, dynamic>>?> getArticles() async {
-    final response = await Dio().get<List<dynamic>>(
-        'https://my-json-server.typicode.com/adamsmaka/json-demo/articles');
+part 'articles_remote_data_source.g.dart';
 
-    final listDynamic = response.data;
+@RestApi(baseUrl: 'https://my-json-server.typicode.com/adamsmaka/json-demo')
+abstract class ArticlesRemoteRetrofitDataSource {
+  factory ArticlesRemoteRetrofitDataSource(Dio dio, {String baseUrl}) =
+      _ArticlesRemoteRetrofitDataSource;
 
-    if (listDynamic == null) {
-      return null;
-    }
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-  }
+  @GET('/articles')
+  Future<List<ArticleModel>> getArticles();
 }
